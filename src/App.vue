83537f44fefd30a23a635d0cffb1de14f998c7ae
CRBroughton <crbroughton@posteo.uk>
2024-04-28 13:34:41 +0100

    feat: :tada: init commit, hello world!

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <canvas class="glslCanvas" data-fragment-url="/shaders/HelloWorld.frag" width="500" height="500"></canvas>
+</template>
+
+<style scoped>
+.logo {
+  height: 6em;
+  padding: 1.5em;
+  will-change: filter;
+  transition: filter 300ms;
+}
+.logo:hover {
+  filter: drop-shadow(0 0 2em #646cffaa);
+}
+.logo.vue:hover {
+  filter: drop-shadow(0 0 2em #42b883aa);
+}
+</style>
